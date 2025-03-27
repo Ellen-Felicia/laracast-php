1d969b2416c8ac01dde48a32b0b883447ac29fc5
Ellen Felícia Ferreira Miranda <ellen.miranda@edu.unimontes.br>
2025-03-27 10:55:43 -0300

    fix: password encrypted

--- views/index.view.php
+++ views/index.view.php
@@ -7,7 +7,7 @@
 
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        Home page 
+        <p> Hello <?= $_SESSION['user']['email'] ?? 'Guest'?>, welcome home page! </p>
     </div>
   </main>
 
